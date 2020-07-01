7b1ca7ce63b9160fd96fe5bac66a662cf61b00aa
Tran Anh Minh <trananhminh910@gmail.com>
2020-07-01 22:38:44 +0700

    Updates

---
+++ precache-manifest.63053ba9ce93903dd0a4a4caa37fa0b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb5cb45b5f183af1aab1e21a7eb0c51",
+    "url": "/sale_mobile/index.html"
+  },
+  {
+    "revision": "0c8b4ad52d61c54b7b59",
+    "url": "/sale_mobile/static/js/2.6f4f7bc7.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/sale_mobile/static/js/2.6f4f7bc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc48fac8fac9bb08074a",
+    "url": "/sale_mobile/static/js/main.aac727e4.chunk.js"
+  },
+  {
+    "revision": "67fcff046dd29e9bdee0",
+    "url": "/sale_mobile/static/js/runtime-main.6e4b496b.js"
+  }
+]);
